7e559a0d5df945b95127e64c3a5259860fc44329
rashidul191 <67516291+rashidul191@users.noreply.github.com>
2020-10-25 23:58:48 +0600

    add some codes

--- src/Components/Home/Footer/Footer.js
+++ src/Components/Home/Footer/Footer.js
@@ -10,40 +10,67 @@ const Footer = () => {
     return (
         <section >
             <div className="footer-area">
-                <div className="container">
-                    <div>
+                <div className="container pt-5">
+                    <div className="row">
+                        <div className="col-md-4">
 
+                        </div>
+                        <div className="col-md-4">
+
+                        </div>
+                        <div className="col-md-4">
+                            <form>
+                                <div class="form-group">
+                                    
+                                    <input type="email" class="form-control"  placeholder="name@example.com" />
+                                </div>
+                                <div>
+                                    <input type="text" class="form-control" placeholder="Enter Your Name"/>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="exampleFormControlTextarea1">Example textarea</label>
+                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                </div>
+
+                                <div className="form-group">
+                                    <button type="submit">Submit</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
 
-                </div>
-            </div>
-          
-                <div className="container ">
-                    <div className="row ">
-                        <div className="col-md-8">
-                            <p className="text-white copy-right"> Copyright {(new Date()).getFullYear()} Rashidul </p>
-                        </div>
 
-                        <div className="col-md-4">
-                            <div className="row ">
-                                <div className="col-md-2">
-                                    <a href="https://www.facebook.com/rashidul191r"> <img className="social-icons" src={facebookIcon} alt="" /></a>
-                                </div>
-                                <div className="col-md-2">
-                                    <a href="https://twitter.com/Rashidul191"><img className="social-icons" src={twitterIcon} alt="" /></a>
-                                </div>
-                                <div className="col-md-2">
-                                    <a href="https://www.linkedin.com/in/rashidul191/"><img className="social-icons" src={linkedinIcon} alt="" /></a>
-                                </div>
-                                <div className="col-md-2">
-                                    <a href="https://github.com/rashidul191/"><img className="social-icons" src={githubIcon} alt="" /></a>
-                                </div>
 
+
+                    <div className="mt-5 pt-5 ">
+                        <div className="row ">
+                            <div className="col-md-8">
+                                <p className=" copy-right"> Copyright {(new Date()).getFullYear()} Rashidul </p>
+                            </div>
+
+                            <div className="col-md-4">
+                                <div className="row ">
+                                    <div className="col-md-2">
+                                        <a href="https://www.facebook.com/rashidul191r"> <img className="social-icons" src={facebookIcon} alt="" /></a>
+                                    </div>
+                                    <div className="col-md-2">
+                                        <a href="https://twitter.com/Rashidul191"><img className="social-icons" src={twitterIcon} alt="" /></a>
+                                    </div>
+                                    <div className="col-md-2">
+                                        <a href="https://www.linkedin.com/in/rashidul191/"><img className="social-icons" src={linkedinIcon} alt="" /></a>
+                                    </div>
+                                    <div className="col-md-2">
+                                        <a href="https://github.com/rashidul191/"><img className="social-icons" src={githubIcon} alt="" /></a>
+                                    </div>
+
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            
+            </div>
+
         </section>
     );
 };
