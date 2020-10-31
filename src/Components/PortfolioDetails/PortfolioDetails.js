de4c377e343c637e78a461233104af70a11ffbfb
rashidul191 <67516291+rashidul191@users.noreply.github.com>
2020-11-01 00:05:01 +0600

    add porfolio some done

--- src/Components/PortfolioDetails/PortfolioDetails.js
+++ src/Components/PortfolioDetails/PortfolioDetails.js
@@ -2,6 +2,7 @@ import React from 'react';
 import creativeAgency from '../../Images/My Work Projects/creative-agency.png';
 import volentarNetwork from '../../Images/My Work Projects/volenter-network.png';
 import travelGuru from '../../Images/My Work Projects/traval-guru.png';
+import learnCodeAcademy from '../../Images/My Work Projects/Online Coures.png';
 import './PortfolioDetails.css';
 
 const PortfolioDetails = () => {
@@ -17,7 +18,7 @@ const PortfolioDetails = () => {
                     <h6 className=""> MY PORTFOLIO </h6>
                     <h3> CHECK OUT MY RECENT PROJECT </h3>
                 </div>
-                <div>
+                <div className="">
                     <div class="card-deck">
                         <div class="card">
                             <a href="https://creative-agency-rashidul.netlify.app/">  <img class="card-img-top" src={creativeAgency} alt="Card image cap" /> </a>
@@ -56,6 +57,49 @@ const PortfolioDetails = () => {
                                     <a href="https://travel-guru-net.netlify.app">  <button className="site-link-btn mr-2">WebSite</button> </a>
                                     <a href="https://github.com/rashidul191/travel-guru"> <button className="site-link-btn">GitHub</button> </a>
                                 </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-5">
+                    <div class="card-deck">
+                        <div class="card">
+                            <a href="https://tender-hermann-4c40fa.netlify.app/">  <img class="card-img-top" src={learnCodeAcademy} alt="Card image cap" /> </a>
+                            <div class="card-body">
+                                <h5 class="card-title">Learn Code Academy</h5>
+                                <p class="card-text">A site is online course service web application. Any User want order buy any online course. and Any teacher want a sell his/her course this website. </p>
+                            </div>
+                            <div class="card-footer">
+                                <div className="text-center">
+                                    <a href="https://tender-hermann-4c40fa.netlify.app/">  <button className="site-link-btn mr-2">WebSite</button> </a>
+                                    <a href="https://github.com/rashidul191/learn-code-academy"> <button className="site-link-btn">GitHub</button> </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            {/* <a href="https://volunteer-network-main.netlify.app/">  <img class="card-img-top" src={volentarNetwork} alt="Card image cap" /> </a> */}
+                            <div class="card-body">
+                                {/* <h5 class="card-title">VOLUNTEER NETWORK (Full-Stack)</h5>
+                                <p class="card-text">A full-stack social service provider web application.A single page web application with a dashboard for a volunteer community.Users can volunteer for a task.Admins can see the volunteer tasks and change/delete them.</p> */}
+                            </div>
+                            <div class="card-footer">
+                                {/* <div className="text-center">
+                                    <a href="https://volunteer-network-main.netlify.app/">  <button className="site-link-btn mr-2">WebSite</button> </a>
+                                    <a href="https://github.com/rashidul191/volunteer-network-main"> <button className="site-link-btn">GitHub</button> </a>
+                                </div> */}
+                            </div>
+                        </div>
+                        <div class="card">
+                          {/* <a href="https://travel-guru-net.netlify.app/">  <img src={travelGuru} class="card-img-top" alt="..." /> </a> */}
+                            <div class="card-body">
+                                {/* <h5 class="card-title">TRAVEL GURU</h5>
+                                <p class="card-text">A travel guru hotel agency web application. A demo web app for booking hotels of 3 different spots. Users can select any spot for booking with a signup process. The hotel suggestion is given according to the selected spot.</p> */}
+                            </div>
+                            <div class="card-footer">
+                                {/* <div className="text-center">
+                                    <a href="https://travel-guru-net.netlify.app">  <button className="site-link-btn mr-2">WebSite</button> </a>
+                                    <a href="https://github.com/rashidul191/travel-guru"> <button className="site-link-btn">GitHub</button> </a>
+                                </div> */}
                             </div>
                         </div>
                     </div>
