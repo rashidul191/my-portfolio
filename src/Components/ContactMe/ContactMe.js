36ece604936b9507932ec09704f5ee941f4b5094
rashidul191 <67516291+rashidul191@users.noreply.github.com>
2020-10-31 23:42:01 +0600

    contact me done

--- src/Components/ContactMe/ContactMe.js
+++ src/Components/ContactMe/ContactMe.js
@@ -13,33 +13,54 @@ const ContactMe = () => {
                 <h3>Contact Me</h3>
             </div>
             <div className="container pt-5 pb-5">
+                <div className="row">
 
+                    <div className="col-md-8">
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <div class="form-group">
+                                <label for="exampleFormControlTextarea1">Name:*</label>
+                                <input type="text" class="form-control" name="name" ref={register({ required: true })} placeholder="Name / Company name" />
+                                {errors.name && <span className="error-text">This field is required</span>}
+                            </div>
 
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div class="form-group">
-                        <label for="exampleFormControlTextarea1">Name:*</label>
-                        <input type="text" class="form-control" name="name" ref={register({ required: true })} placeholder="Name / Company name" />
-                        {errors.name && <span className="error-text">This field is required</span>}
+                            <div class="form-group">
+                                <label for="exampleFormControlTextarea1">Email:*</label>
+                                <input type="email" class="form-control" name="email" ref={register({ required: true })} placeholder="email@gmail.com" />
+                                {errors.email && <span className="error-text">This field is required</span>}
+                            </div>
+
+                            <div class="form-group">
+                                <label for="exampleFormControlTextarea1">Phone:*</label>
+                                <input type="text" class="form-control" name="phone" ref={register({ required: true })} placeholder="Enter phone number" />
+                                {errors.phone && <span className="error-text">This field is required</span>}
+                            </div>
+
+                            <div class="form-group">
+                                <label for="exampleFormControlTextarea1">Type your massage</label>
+                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                            </div>
+                            <input className="send-btn" type="submit" value="Send" />
+                        </form>
+
                     </div>
+                    <div className="col-md-4">
+                        <div className="address-me">
+                            <div className="address-box">
+                                <h3>Address</h3>
+                                <h5>Mirpur, Dhaka, Bangladesh</h5>
+                            </div>
+                            <div className="address-box">
+                                <h3>Phone</h3>
+                                <h5>+880 1629 22 60 69</h5>
+                            </div>
+                            <div className="address-box">
+                                <h3>Email</h3>
+                                <h5>md.rashidul371@gmail.com</h5>
+                            </div>
 
-                    <div class="form-group">
-                        <label for="exampleFormControlTextarea1">Email:*</label>
-                        <input type="email" class="form-control" name="email" ref={register({ required: true })} placeholder="email@gmail.com" />
-                        {errors.email && <span className="error-text">This field is required</span>}
+                        </div>
                     </div>
-
-                    <div class="form-group">
-                        <label for="exampleFormControlTextarea1">Phone:*</label>
-                        <input type="text" class="form-control" name="phone" ref={register({ required: true })} placeholder="Enter phone number" />
-                        {errors.phone && <span className="error-text">This field is required</span>}
-                    </div>
-
-                    <div class="form-group">
-                        <label for="exampleFormControlTextarea1">Type your massage</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                    </div>
-                    <input className="send-btn" type="submit" value="Send" />
-                </form>
+                </div>
             </div>
         </section>
     );
--- src/Components/Home/About/About.js
+++ src/Components/Home/About/About.js
@@ -15,11 +15,11 @@ const About = () => {
                 <div className="row pt-5">
                     <div className="col-md-8 pt-5">
                         <h2>Md Rashidul Islam</h2>
-                        <h5>Web Developer</h5>
+                        <h5 className="text-animation">Web Developer</h5>
                         <p>Hello everyone,my name is Md Rashidul Islam, consectetur adipiscing elit. Etiam suscipit mi eget dui venenatis posuere. In varius nulla non elementum hendrerit. Suspendisse varius sapien odio, in gravida ipsum luctus id. Ut nisl erat, dignissim sed volutpat at, hendrerit imperdiet purus. Maecenas fringilla, augue sed vulputate ullamcorper, risus lorem facilisis massa, a varius nunc tellus sed odio.</p>
                         <p>Suspendisse fermentum lobortis elit sit amet vehicula. Etiam in dolor vitae leo euismod consequat a vitae ligula.Nunc viverra enim purus, vel luctus ante tempus vel. Vivamus et metus massa. Pellentesque at nulla velit. Morbi pellentesque egestas massa in scelerisque. Vivamus pharetra dolor enim, vel vulputate erat euismod sit amet.</p>
                         <div className="pt-3">
-                            <a href="https://drive.google.com/file/d/163zEw4V0DUeRFnh3gcs44hrB3nkc_FR9/view"> <button className="header-btn-cv">  DOWNLOAD CV </button> </a>
+                            <a href="https://drive.google.com/u/0/uc?id=163zEw4V0DUeRFnh3gcs44hrB3nkc_FR9&export=download"> <button className="header-btn-cv">  DOWNLOAD CV </button> </a>
                         </div>
                     </div>
                     <div className="col-md-3">
