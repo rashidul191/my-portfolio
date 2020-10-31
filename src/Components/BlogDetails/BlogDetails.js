34b4b85294595ed671e0208beb0865efdbdda230
rashidul191 <67516291+rashidul191@users.noreply.github.com>
2020-10-31 23:45:57 +0600

    blog details some done

--- src/Components/BlogDetails/BlogDetails.js
+++ src/Components/BlogDetails/BlogDetails.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import './BlogDetails.css';
 
 const BlogDetails = () => {
     return (
-        <div>
-            <h3>blog details coming soon........</h3>
-        </div>
+        <section>
+            <div className="blog-header mt-5 pt-4">
+                <h3>My Blogs</h3>
+            </div>
+            <div className="container pt-5">
+                <div className="text-center">
+
+                    <h3>Latest News</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci autem suscipit id laudantium doloribus saepe ea architecto deleniti ad laboriosam! Facilis, quisquam sequi magni aliquam id debitis illum enim atque.</p>
+                </div>
+                <div>
+
+                </div>
+
+
+            </div>
+        </section>
     );
 };
 
