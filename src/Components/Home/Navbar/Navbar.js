533e243f1d85734ccd4657baf20600a6ab7f27c6
rashidul191 <67516291+rashidul191@users.noreply.github.com>
2020-12-09 11:10:32 +0600

    update navbar sticky-top

--- src/Components/Home/Navbar/Navbar.js
+++ src/Components/Home/Navbar/Navbar.js
@@ -5,13 +5,11 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <section >
-            <div className="nav-style">
-
-
-                <div className="container">
+        <section className="sticky-top">
+            <div className="nav-style ">
+                <div className="container ">
                     <nav class="navbar navbar-expand-lg navbar-light ">
-                       <Link to="/"> <a class="navbar-brand" href="#"><img className="main-logo" src={logo} alt="" /></a> </Link>
+                        <Link to="/"> <a class="navbar-brand" href="#"><img className="main-logo" src={logo} alt="" /></a> </Link>
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
@@ -19,37 +17,37 @@ const Navbar = () => {
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul class="navbar-nav ml-auto">
                                 <li class="nav-item mr-3 active pt-2">
-                                  <Link to="/home"> <a class="nav-link nav-text">HOME </a> </Link>
+                                    <Link to="/home"> <a class="nav-link nav-text">HOME </a> </Link>
                                 </li>
 
                                 <li class="nav-item mr-3 active pt-2">
-                                  <Link to="/about"> <a class="nav-link nav-text">ABOUT </a> </Link>
+                                    <Link to="/about"> <a class="nav-link nav-text">ABOUT </a> </Link>
                                 </li>
 
                                 <li class="nav-item mr-3 active pt-2">
-                                   <Link to="/skill"> <a class="nav-link nav-text">SKILL</a> </Link>
+                                    <Link to="/skill"> <a class="nav-link nav-text">SKILL</a> </Link>
                                 </li>
 
                                 <li class="nav-item mr-3 active pt-2">
                                     <Link to="/service"> <a class="nav-link nav-text">SERVICE</a> </Link>
                                 </li>
 
-                               
+
 
                                 <li class="nav-item mr-3 active pt-2" >
                                     <Link to="/portfolio"> <a class="nav-link nav-text">PORTFOLIO</a> </Link>
                                 </li>
 
-                               
+
 
                                 <li class="nav-item mr-3 active pt-2" >
-                                   <Link to="/blog"> <a class="nav-link nav-text">BLOG </a> </Link>
+                                    <Link to="/blog"> <a class="nav-link nav-text">BLOG </a> </Link>
                                 </li>
 
-                               
+
 
                                 <li class="nav-item mr-3 active pt-2" >
-                                   <Link to="/contact-me"> <a class="nav-link nav-text">CONTACT ME </a> </Link>
+                                    <Link to="/contact-me"> <a class="nav-link nav-text">CONTACT ME </a> </Link>
                                 </li>
 
                                 <li class="nav-item mr-3 active" >
